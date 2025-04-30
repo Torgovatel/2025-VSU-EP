291b9dee9dd7ac893593f9fdc6a8a15a6dd23864
torgovatel <andrey.kazanin.02@mail.ru>
2025-04-30 23:43:42 +0300

    fix: ручка переведена на русский язык

--- src/main/java/ru/vsu/practice/demo/HelloController.java
+++ src/main/java/ru/vsu/practice/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, hihi!";
+        return "Привет, дорогой друг!";
     }
 
     @GetMapping("/greet")
