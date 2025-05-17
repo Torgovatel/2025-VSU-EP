dafe363289a03b28d4ca726b27a96691cc0d78a6
torgovatel <andrey.kazanin.02@mail.ru>
2025-05-17 15:14:44 +0300

    feat: обновил UserController под новую версию сервиса

--- web/src/main/java/ru/vsu/practice/demo/UserController.java
+++ web/src/main/java/ru/vsu/practice/demo/UserController.java
@@ -1,33 +1,69 @@
-//package ru.vsu.practice.demo;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/users")
-//public class UserController {
-//
-//    private final UserService userService;
-//
-//    @Autowired
-//    public UserController(UserService userService) {
-//        this.userService = userService;
-//    }
-//
-//    @PostMapping
-//    public User addUser(@RequestParam("name") String name, @RequestParam("age") int age) {
-//        return userService.addUser(name, age);
-//    }
-//
-//    @GetMapping
-//    public List<User> getUsers() {
-//        return userService.getUsers();
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public boolean deleteUser(@PathVariable("id") long id) {
-//        return userService.deleteUser(id);
-//    }
-//}
+package ru.vsu.practice.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController() {
+        // Путь к файлу можно вынести в конфиг, если нужно
+        this.userService = new UserService("users.json");
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers(@RequestParam Map<String, String> filters) {
+        return ResponseEntity.ok(userService.getAll(filters));
+    }
+
+    @GetMapping("/user/{uid}")
+    public ResponseEntity<User> getUserById(@PathVariable String uid) {
+        return ResponseEntity.ok(userService.getById(uid));
+    }
+
+    @GetMapping("/user/{uid}/friends")
+    public ResponseEntity<List<User>> getUserFriends(
+            @PathVariable String uid,
+            @RequestParam Map<String, String> filters
+    ) {
+        return ResponseEntity.ok(userService.getFriends(uid, filters));
+    }
+
+    @DeleteMapping("/user/{uid}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String uid) {
+        userService.delete(uid);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User created = userService.create(user);
+        return ResponseEntity.ok(created);
+    }
+
+    @PutMapping("/user/{uid}")
+    public ResponseEntity<User> updateUser(@PathVariable String uid, @RequestBody User patch) {
+        User updated = userService.update(uid, patch);
+        return ResponseEntity.ok(updated);
+    }
+
+    @PatchMapping("/user/{uid}/friends/add")
+    public ResponseEntity<Void> addFriend(@PathVariable String uid, @RequestBody Map<String, String> payload) {
+        String friendUid = payload.get("friendUid");
+        userService.addFriend(uid, friendUid);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/user/{uid}/friends/rm")
+    public ResponseEntity<Void> removeFriend(@PathVariable String uid, @RequestBody Map<String, String> payload) {
+        String friendUid = payload.get("friendUid");
+        userService.removeFriend(uid, friendUid);
+        return ResponseEntity.noContent().build();
+    }
+}
